69351524d4918b0f2b224ee7b41f6563c6987d52
hafida <oumnawfal@gmail.com>
2016-04-07 12:05:29 +0200

    correction du sabonner.js: oublie d'une autre parenthèse

--- js/sabonner.js
+++ js/sabonner.js
@@ -1,6 +1,6 @@
 console.log('backgroundColor :' + $('.sabonner').css("background-color"));
 $('#btn_sabonner').click(function () {
 	 $(this).css("background-color","green");
-	 console.log('backgroundColor :' + $('.sabonner').css("background-color");
+	 console.log('backgroundColor :' + $('.sabonner').css("background-color"));
 
 });
