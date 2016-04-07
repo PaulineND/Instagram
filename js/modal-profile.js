44ef4c9184ed577a950f1a5bbdf792ed7de81373
Nossyra46 <nossyra@gmail.com>
2016-04-07 11:12:19 +0200

    started working on modal box for the profile

---
+++ js/modal-profile.js
@@ -0,0 +1,19 @@
+var susp = document.getElementById('points');
+var bodyProfile = document.getElementById('body');
+
+function createProfile(e) {
+	// création des éléments html
+	var modalProfile = document.createElement('div');
+
+	// ajout des attributs
+	modalProfile.setAttribute('class', 'modal-picture');
+
+	// ajout des textes
+
+	// insertion dans le dom
+
+
+	// effacer du dom
+}
+
+susp.addEventListener('click', createProfile);
