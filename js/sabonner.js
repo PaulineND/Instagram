274ab0894c0d93e20c08be251392638bdca0a62c
hafida <oumnawfal@gmail.com>
2016-04-07 11:44:01 +0200

    conflict on gitignore

---
+++ js/sabonner.js
@@ -0,0 +1,6 @@
+console.log('backgroundColor :' + $('.sabonner').css("background-color");
+$('.sabonner').click(function () {
+	 $(this).css("background-color","green");
+	 console.log('backgroundColor :' + $('.sabonner').css("background-color");
+
+});
